41c9f77e494934576c3b36a20b59b8a823e6aef3
thehilmisu <ozcanhakanzi@gmail.com>
2026-02-06 00:26:37 +0100

    played with the menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,167 +7,110 @@
 
 namespace gobjs = gameobjects;
 
-namespace game {
+namespace game
+{
 
-void exitGame() {
+  void exitGame()
+  {
     Window &window = Window::getInstance();
     window.setIsRunning(false);
-}
-
-game::MainMenuActions mainMenu() {
-  Window &window = Window::getInstance();
-  Gui &gui = Gui::getInstance();
-
-  window.setCursorInputMode(CURSOR_NORMAL);
-
-  window.getCamera().pitch = 0.0f;
-  window.getCamera().yaw = 0.0f;
-  window.getCamera().position = glm::vec3(0.0f);
-
-  gobjs::Player player(glm::vec3(14.0f, -8.0f, -40.0f));
-
-  float dt = 0.0f;
-  float totalTime = 0.0f;
-  std::string skybox = "nightskybox";
-  while (!window.shouldClose()) {
-    float start = getTime();
-
-    gui.newFrame();
-
-    // Update perspective matrix
-    window.updatePerspectiveMat(FOVY, ZNEAR, ZFAR, window.getWidth(),
-                                window.getHeight());
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    gfx::displayPlayerPlane(totalTime, player.transform, player.getPlayerObj());
-
-    // Display skybox
-    gfx::displaySkybox(skybox);
-
-    game::MainMenuActions selected = gui.drawMainMenu();
-
-    player.transform.rotation.y += 0.5f * dt;
-
-    gui.render();
-
-    window.updateKeyStates();
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    window.swapBuffers();
-    window.pollEvents();
-    gfx::outputErrors();
-    totalTime += dt;
-
-    switch (selected) {
-    case OPTIONS: // Fall through
-    case START_GAME:
-      return selected;
-    case CREDITS:
-      break;
-    case CHANGE_PLANE_MINUS: {
-      int c = player.getCurrentIndex();
-      c = (c - 1) % 2;
-      player.setPlayerObj(c);
-      skybox = "skybox";
-      break;
-    }
-    case CHANGE_PLANE_PLUS: {
-      int c = player.getCurrentIndex();
-      c = (c + 1) % 2;
-      player.setPlayerObj(c);
-      skybox = "nightskybox";
-      break;
-    }
-    case EXIT_GAME: {
-      return selected;    
-    }
-    default:
-      break;
-    }
-
-    dt = getTime() - start;
   }
 
-  return NONE_SELECTED;
-}
+  game::MainMenuActions mainMenu()
+  {
+    Window &window = Window::getInstance();
+    Gui &gui = Gui::getInstance();
 
-// void highScoreTableScreen(const HighScoreTable &highscores)
-// {tty-cmd{"ok": true, "data": "18"}\x
-// 	State* state = State::get();
+    window.setCursorInputMode(CURSOR_NORMAL);
 
-// 	glfwSetInputMode(state->getWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    window.getCamera().pitch = 0.0f;
+    window.getCamera().yaw = 0.0f;
+    window.getCamera().position = glm::vec3(0.0f);
 
-// 	state->getCamera().pitch = 0.0f;
-// 	state->getCamera().yaw = 0.0f;
-// 	state->getCamera().position = glm::vec3(0.0f);
+    gobjs::Player player(glm::vec3(14.0f, -8.0f, -40.0f));
+    gobjs::Enemy enemy(glm::vec3(0.0f, 50.0f, -100.0f), 2, 100);
+    enemy.transform.rotation.x = glm::radians(40.0f);
+    std::vector<gobjs::Enemy> enemies = {enemy};
 
-// 	bool quit = false;
-// 	while(!glfwWindowShouldClose(state->getWindow()) && !quit) {
-// 		nk_glfw3_new_frame(state->getNkGlfw());
+    float dt = 0.0f;
+    float totalTime = 0.0f;
+    std::string skybox = "nightskybox";
+    float mRotation = enemy.transform.rotation.y;
+    float rotationDirection = 1.0f;
+    while (!window.shouldClose())
+    {
+      float start = getTime();
 
-// 		//Update perspective matrix
-// 		state->updatePerspectiveMat(FOVY, ZNEAR, ZFAR);
+      gui.newFrame();
 
-// 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      // Update perspective matrix
+      window.updatePerspectiveMat(FOVY, ZNEAR, ZFAR, window.getWidth(),
+                                  window.getHeight());
 
-// 		//Display skybox
-// 		gfx::displaySkybox();
-// 		//GUI
-// 		quit = gui::displayHighScores(highscores);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-// 		state->updateKeyStates();
-// 		nk_glfw3_render(state->getNkGlfw(), NK_ANTI_ALIASING_ON, 512 *
-// 1024, 128 * 1024); 		glEnable(GL_CULL_FACE); 		glEnable(GL_DEPTH_TEST);
-// 		glEnable(GL_BLEND);
-// 		glfwSwapBuffers(state->getWindow());
-// 		glfwPollEvents();
-// 		gfx::outputErrors();
-// 	}
-// }
+      gfx::displayPlayerPlane(totalTime, player.transform, player.getPlayerObj());
+      gfx::displayPlanes(totalTime, enemies);
 
-// void settingsScreen()
-// {
-// 	State* state = State::get();
-// 	SettingsValues values = GlobalSettings::get()->values;
-// 	game::SettingsScreenAction action = game::SETTINGS_NONE_SELECTED;
+      // Display skybox
+      gfx::displaySkybox(skybox);
 
-// 	glfwSetInputMode(state->getWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+      game::MainMenuActions selected = gui.drawMainMenu();
 
-// 	state->getCamera().pitch = 0.0f;
-// 	state->getCamera().yaw = 0.0f;
-// 	state->getCamera().position = glm::vec3(0.0f);
+      
+      mRotation += 0.1f * dt * rotationDirection;
+      if (mRotation > glm::radians(60.0f) || mRotation < glm::radians(-60.0f))
+        rotationDirection *= -1.0f;
+      
+      for (auto &e : enemies) e.transform.rotation.y = mRotation;
+      
+      player.transform.rotation.y = mRotation;
+   
+      gui.render();
 
-// 	bool quit = false;
-// 	while(!glfwWindowShouldClose(state->getWindow()) && !quit) {
-// 		nk_glfw3_new_frame(state->getNkGlfw());
+      window.updateKeyStates();
+      glEnable(GL_CULL_FACE);
+      glEnable(GL_DEPTH_TEST);
+      glEnable(GL_BLEND);
+      window.swapBuffers();
+      window.pollEvents();
+      gfx::outputErrors();
+      totalTime += dt;
 
-// 		//Update perspective matrix
-// 		state->updatePerspectiveMat(FOVY, ZNEAR, ZFAR);
+      switch (selected)
+      {
+      case OPTIONS: // Fall through
+      case START_GAME:
+        return selected;
+      case CREDITS:
+        break;
+      case CHANGE_PLANE_MINUS:
+      {
+        int c = player.getCurrentIndex();
+        c = (c - 1) % 2;
+        player.setPlayerObj(c);
+        skybox = "skybox";
+        break;
+      }
+      case CHANGE_PLANE_PLUS:
+      {
+        int c = player.getCurrentIndex();
+        c = (c + 1) % 2;
+        player.setPlayerObj(c);
+        skybox = "nightskybox";
+        break;
+      }
+      case EXIT_GAME:
+      {
+        return selected;
+      }
+      default:
+        break;
+      }
 
-// 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      dt = getTime() - start;
+    }
 
-// 		//Display skybox
-// 		gfx::displaySkybox();
-// 		//Display settings
-// 		action = gui::displaySettingsMenu(values);
-
-// 		state->updateKeyStates();
-// 		nk_glfw3_render(state->getNkGlfw(), NK_ANTI_ALIASING_ON, 512 *
-// 1024, 128 * 1024); 		glEnable(GL_CULL_FACE); 		glEnable(GL_DEPTH_TEST);
-// 		glEnable(GL_BLEND);
-// 		glfwSwapBuffers(state->getWindow());
-// 		glfwPollEvents();
-// 		gfx::outputErrors();
-
-// 		if(action != game::SETTINGS_NONE_SELECTED)
-// 			quit = true;
-// 	}
-
-// 	if(action == game::SAVE_SETTINGS) {
-// 		GlobalSettings::get()->values = values;
-// 		alListenerf(AL_GAIN, GlobalSettings::get()->values.volume);
-// 	}
-// }
+    return NONE_SELECTED;
+  }
 } // namespace game
